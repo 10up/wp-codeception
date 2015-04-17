42a0dd05551feab436ad8e97f481ba6a5626398f
Eugene Manuilov <eugene.manuilov@10up.com>
2015-04-17 10:33:23 +0300

    Reworked WordPress module. Moved previous methods to the helper class and extended WordPress module from Framework module.

---
+++ classes/WPCC/Component/Connector/WordPress.php
@@ -0,0 +1,89 @@
+<?php
+
+// +----------------------------------------------------------------------+
+// | Copyright 2015 10up Inc                                              |
+// +----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License, version 2, as  |
+// | published by the Free Software Foundation.                           |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to the Free Software          |
+// | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,               |
+// | MA 02110-1301 USA                                                    |
+// +----------------------------------------------------------------------+
+
+namespace WPCC\Component\Connector;
+
+use Symfony\Component\BrowserKit\Client;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\BrowserKit\Response;
+
+/**
+ * WordPress connector simulates a browser and is used by WordPress module.
+ *
+ * @since 1.0.0
+ * @category WPCC
+ * @package Component
+ * @subpackage Connector
+ */
+class WordPress extends Client {
+
+	use \Codeception\Lib\Connector\Shared\PhpSuperGlobalsConverter;
+
+    /**
+     * Makes a request.
+     *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+     * @param \Symfony\Component\BrowserKit\Request $request An origin request instance.
+     * @return \Symfony\Component\BrowserKit\Response An origin response instance.
+     */
+	protected function doRequest( Request $request ) {
+        $_COOKIE = $request->getCookies();
+		$_SERVER = $request->getServer();
+		$_FILES = $this->remapFiles( $request->getFiles() );
+
+		$_REQUEST = $this->remapRequestParameters( $request->getParameters() );
+		if ( strtoupper( $request->getMethod() ) == 'GET' ) {
+			$_GET = $_REQUEST;
+		} else {
+			$_POST = $_REQUEST;
+		}
+
+		$uri = str_replace( 'http://localhost', '', $request->getUri() );
+
+		$_SERVER['REQUEST_METHOD'] = strtoupper( $request->getMethod() );
+		$_SERVER['REQUEST_URI'] = $uri;
+
+		ob_start();
+//		include $this->index;
+		$content = ob_get_contents();
+		ob_end_clean();
+
+		$headers = array();
+		$php_headers = headers_list();
+		foreach ( $php_headers as $value ) {
+			// Get the header name
+			$parts = explode( ':', $value );
+			if ( count( $parts ) > 1 ) {
+				$name = trim( array_shift( $parts ) );
+				// Build the header hash map
+				$headers[ $name ] = trim( implode( ':', $parts ) );
+			}
+		}
+
+		$headers['Content-type'] = isset( $headers['Content-type'] ) 
+			? $headers['Content-type']
+			: "text/html; charset=UTF-8";
+
+		return new Response( $content, 200, $headers );
+	}
+
+}
--- classes/WPCC/Module/WordPress.php
+++ classes/WPCC/Module/WordPress.php
@@ -20,6 +20,9 @@
 
 namespace WPCC\Module;
 
+use Codeception\TestCase;
+use WPCC\Component\Connector\WordPress as WordPressConnector;
+
 /**
  * WordPress module.
  *
@@ -27,105 +30,18 @@ namespace WPCC\Module;
  * @category WPCC
  * @package Module
  */
-class WordPress extends \Codeception\Module {
+class WordPress extends \Codeception\Lib\Framework {
 
 	/**
-	 * The array of temporary created users.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @access protected
-	 * @var array
-	 */
-	protected $_temp_users = array();
-
-	/**
-	 * Deletes temp user.
+	 * Initializes module before test run.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @access public
-	 * @param \WP_User|int $user The user object or ID.
+	 * @param \Codeception\TestCase $test The current test case object.
 	 */
-	public function deleteTempUser( $user ) {
-		if ( is_a( $user, '\WP_User' ) ) {
-			$user = $user->ID;
-		}
-		
-		$user_index = array_search( $user, $this->_temp_users );
-		if ( false !== $user_index ) {
-			if ( is_multisite() ) {
-				wpmu_delete_user( $user );
-			} else {
-				wp_delete_user( $user );
-			}
-
-			$this->debug( 'Deleted temporary user with id = ' . $user );
-
-			unset( $this->_temp_users[ $user_index ] );
-		}
+	public function _before( TestCase $test ) {
+		$this->client = new WordPressConnector();
 	}
-
-	/**
-	 * Deletes all temp users.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @access public
-	 */
-	public function deleteTempUsers() {
-		foreach ( $this->_temp_users as $user_id ) {
-			$this->deleteTempUser( $user_id );
-		}
-
-		$this->_temp_users = array();
-	}
-
-	/**
-	 * Creates temp user.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @access public
-	 * @param string $role A user role to set.
-	 * @param string $password A password to set.
-	 * @return \WP_User|\WP_Error The user object on success, otherwise WP_Error object.
-	 */
-	public function createTempUser( $role = 'administrator', $password = 'qwerty' ) {
-		$faker = \Faker\Factory::create();
-
-		$first_name = $faker->firstName;
-		$last_name = $faker->lastName;
-		$display_name = sprintf( '%s %s', $first_name, $last_name );
-
-		$user_data = array(
-			'user_pass'    => $password,
-			'user_login'   => sanitize_title( $display_name ),
-			'user_email'   => $faker->email,
-			'display_name' => $display_name,
-			'first_name'   => $first_name,
-			'last_name'    => $last_name,
-			'role'         => $role,
-		);
-
-		$user_id = wp_insert_user( $user_data );
-		if ( ! is_wp_error( $user_id ) ) {
-			$this->_temp_users[] = $user_id;
-			add_user_to_blog( get_current_blog_id(), $user_id, $role );
-
-			$message = sprintf(
-				'Created temporary user %s (%s) with %s role',
-				$display_name,
-				$user_id,
-				$role
-			);
-			
-			$this->debug( $message );
-			
-			return get_user_by( 'id', $user_id );
-		}
-
-		return $user_id;
-	}
-
+	
 }
