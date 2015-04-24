b4605ad880cfe9fdfc9e5eb579712564a15c006b
Eugene Manuilov <eugene.manuilov@10up.com>
2015-04-24 17:05:09 +0300

    Added assertions for user and post metas.

--- classes/WPCC/Module/WordPress.php
+++ classes/WPCC/Module/WordPress.php
@@ -38,14 +38,83 @@ class WordPress extends \Codeception\Module {
 	 * @access public
 	 * @param int $user_id The user id.
 	 * @param string $meta_key The meta key to check.
-	 * @param string $meta_value The meta value to check
+	 * @param mixed $meta_value The meta value to check
 	 */
 	public function seeUserMetaFor( $user_id, $meta_key, $meta_value = null ) {
 		$metas = get_user_meta( $user_id, $meta_key );
-		$this->assertNotEmpty( $metas, 'User meta does not exist' );
+
+		$message = sprintf( 'User meta %s does not exist', $meta_key );
+		$this->assertNotEmpty( $metas, $message );
 
 		if ( func_num_args() > 2 ) {
-			$this->assertContains( $meta_value, $metas, 'User does not have expected meta' );
+			$message = sprintf( 'User meta %s does not contain expected value', $meta_key );
+			$this->assertContains( $meta_value, $metas, $message );
+		}
+	}
+
+	/**
+	 * Checks if user meta doesn't exists.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 * @param int $user_id The user id.
+	 * @param string $meta_key The meta key to check.
+	 * @param mixed $meta_value The meta value to check
+	 */
+	public function dontSeeUserMetaFor( $user_id, $meta_key, $meta_value = null ) {
+		$metas = get_user_meta( $user_id, $meta_key );
+
+		if ( func_num_args() > 2 ) {
+			$message = sprintf( 'User meta %s contains not expected value', $meta_key );
+			$this->assertNotContains( $meta_value, $metas, $message );
+		} else {
+			$message = sprintf( 'User meta %s is not empty', $meta_key );
+			$this->assertEmpty( $metas, $message );
+		}
+	}
+
+	/**
+	 * Checks a post meta exists for a post.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 * @param int $post_id The post id.
+	 * @param string $meta_key The meta key to check.
+	 * @param mixed $meta_value The meta value to check
+	 */
+	public function seePostMetaFor( $post_id, $meta_key, $meta_value = null ) {
+		$metas = get_post_meta( $post_id, $meta_key );
+
+		$message = sprintf( 'Post meta %s does not exist', $meta_key );
+		$this->assertNotEmpty( $metas, $message );
+
+		if ( func_num_args() > 2 ) {
+			$message = sprintf( 'Post meta %s does not contain expected value', $meta_key );
+			$this->assertContains( $meta_value, $metas, $message );
+		}
+	}
+
+	/**
+	 * Checks if post meta doesn't exists.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 * @param int $post The post id.
+	 * @param string $meta_key The meta key to check.
+	 * @param mixed $meta_value The meta value to check
+	 */
+	public function dontSeePostMetaFor( $post, $meta_key, $meta_value = null ) {
+		$metas = get_post_meta( $post, $meta_key );
+
+		if ( func_num_args() > 2 ) {
+			$message = sprintf( 'Post meta %s contains not expected value', $meta_key );
+			$this->assertNotContains( $meta_value, $metas, $message );
+		} else {
+			$message = sprintf( 'Post meta %s is not empty', $meta_key );
+			$this->assertEmpty( $metas, $message );
 		}
 	}
 
