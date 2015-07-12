5ac86346715ba03d3ee2c68f2a982f6218460576
Eugene Manuilov <eugene.manuilov@get10up.com>
2015-07-12 15:08:02 +0300

    Added mime-type detection and attachment metadata generation during attachment creation.

--- classes/WPCC/Component/Factory/Attachment.php
+++ classes/WPCC/Component/Factory/Attachment.php
@@ -40,6 +40,20 @@ class Attachment extends Post {
 	 * @return int The newly created attachment's ID on success, otherwise 0.
 	 */
 	protected function _createObject( $args = array() ) {
+		if ( empty( $args['post_mime_type'] ) ) {
+			if ( ! empty( $args['file'] ) && is_readable( $args['file'] ) ) {
+				$this->_debug( 'Reading mime type of the file: ' . $args['file'] );
+
+				$filetype = wp_check_filetype( basename( $args['file'] ), null );
+				if ( ! empty( $filetype['type'] ) ) {
+					$args['post_mime_type'] = $filetype['type'];
+					$this->_debug( 'Mime type found: ' . $filetype['type'] );
+				} else {
+					$this->_debug( 'Mime type not found' );
+				}
+			}
+		}
+
 		$attachment_id = wp_insert_attachment( $args );
 		if ( $attachment_id ) {
 			$this->_debug(
@@ -47,10 +61,12 @@ class Attachment extends Post {
 				$attachment_id,
 				! empty( $args['file'] ) ? $args['file'] : 'not-provided'
 			);
+
+			$this->_debug( 'Generating attachment metadata' );
+			wp_generate_attachment_metadata( $attachment_id, $args['file'] );
 		} else {
 			$this->_debug( 'Attachment generation failed' );
 		}
-
 
 		return $attachment_id;
 	}
